117e6562c49e5ab75d377b329fb72e85902031d2
Nujood <shawar.nujood@gmail.com>
2020-09-24 23:55:12 +0300

    setting up the discord Bot and login into my created Server

---
+++ src/bot.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const { Client, Message } = require("discord.js");
+
+const bot_client = new Client();
+
+// checking for Ready event
+bot_client.on("ready", () => {
+  console.log(`${bot_client.user.tag} has logged in.`);
+});
+
+bot_client.on("message", (message) => {
+  console.log(`[${message.author.tag}]: ${message.content}`);
+  
+});
+//connect into discord
+bot_client.login(process.env.DISCORDJS_BOT_TOKEN);
